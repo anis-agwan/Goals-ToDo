a09132a04533ca9615d53d790c7b5b7787d681ff
Mohmed Anis <anishagwan@gmail.com>
2020-11-12 22:31:46 +0530

    Created taskpage UI, finished with both page UI, left with functionalities

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:goals_todo/screens/task_page.dart';
 import 'package:goals_todo/widgets.dart';
 
 class Homepage extends StatefulWidget {
@@ -15,7 +16,6 @@ class _HomepageState extends State<Homepage> {
           width: double.infinity,
           padding: EdgeInsets.symmetric(
             horizontal: 24.0,
-            vertical: 32.0,
           ),
           color: Color(0xFFF6F6F6),
           child: Stack(
@@ -26,6 +26,7 @@ class _HomepageState extends State<Homepage> {
                   Container(
                     margin: EdgeInsets.only(
                       bottom: 32.0,
+                      top: 32.0
                     ),
                     child: Image(
                       image: AssetImage(
@@ -33,26 +34,51 @@ class _HomepageState extends State<Homepage> {
                       ),
                     ),
                   ),
-                  TaskCardWidget(
-                    title: "Hello, Friend",
-                    desc: "Welcome to the goals App for daily productive, This is a simple and minimalist app that won't bother you with notifcations or anything."
-                  ),
-                  TaskCardWidget(),
+                  Expanded(
+                    child: ScrollConfiguration(
+                      behavior: NoGlowBehaviour(),
+                      child: ListView(
+                        children: [
+                          TaskCardWidget(
+                              title: "Hello, Friend",
+                              desc: "Welcome to the goals App for daily productive, This is a simple and minimalist app that won't bother you with notifcations or anything."
+                          ),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
               Positioned(
-                bottom: 0.0,
+                bottom: 24.0,
                 right: 0.0,
-                child: Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: Color(0xFF7349FF)
-                  ),
-                  child: Image(
-                    image: AssetImage(
-                      "assets/images/add_icon.png",
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TaskPage()
+                        ),
+                      );
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      gradient: LinearGradient(
+                        colors: [Color(0xFF7349FE), Color(0xFF643FDB)],
+                        begin: Alignment(0.0, -1.0),
+                        end: Alignment(0.0, 1.0),
+                      )
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                        "assets/images/add_icon.png",
+                      ),
                     ),
                   ),
                 ),
---
+++ lib/screens/task_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:goals_todo/widgets.dart';
+
+class TaskPage extends StatefulWidget {
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 24.0,
+          ),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 24.0,
+                      bottom: 6.0,
+                    ),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(24.0),
+                            child: Image(
+                              image: AssetImage(
+                                'assets/images/back_arrow_icon.png'
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Enter Tasks Title",
+                                border: InputBorder.none,
+                              ),
+                              style: TextStyle(
+                                fontSize: 26.0,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF211551),
+                              ),
+                            ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 12.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Enter description of tasks",
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 24.0,
+                        )
+                      ),
+                    ),
+                  ),
+                  ToDO(
+                    text: "Create 1st Todo",
+                    isDone: false,
+                  ),
+                  ToDO(
+                    text: "Create 1st Todo",
+                    isDone: true,
+                  ),
+                  ToDO(
+                    text: "Create 1st Todo",
+                    isDone: false,
+                  ),
+                  ToDO(
+                    isDone: true,
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: 24.0,
+                right: 0.0,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => TaskPage()
+                      ),
+                    );
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Color(0xFFFE3577)
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                        "assets/images/delete_icon.png",
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
